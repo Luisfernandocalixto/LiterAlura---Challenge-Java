c22ea8cda34b66dc96b7afbf7275d28f6ef1f889
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-20 20:19:53 -0600

    Preparando DatosAutor

--- src/main/java/com/alura/literatura/model/DatosAutor.java
+++ src/main/java/com/alura/literatura/model/DatosAutor.java
@@ -1,2 +1,13 @@
-package com.alura.literatura.model;public record DatosAutor() {
+package com.alura.literatura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+        @JsonAlias("name") String nombre,
+        @JsonAlias("birth_year") Integer anioNacimiento,
+        @JsonAlias("death_year") Integer anioFallecimiento
+) {
+
 }

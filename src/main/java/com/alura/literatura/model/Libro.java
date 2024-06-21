d7854dc9952690cc7e31b48b358e79e41d23cf4f
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-20 20:20:18 -0600

    Preparando Libro

--- src/main/java/com/alura/literatura/model/Libro.java
+++ src/main/java/com/alura/literatura/model/Libro.java
@@ -1,2 +1,85 @@
-package com.alura.literatura.model;public class Libro {
+package com.alura.literatura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String titulo;
+
+    private String idioma;
+    private Integer descargas;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    // Constructores, getters y setters
+
+    public Libro() {
+    }
+
+    public Libro(DatosLibro datosLibro) {
+        this.titulo = datosLibro.titulo();
+        if (datosLibro.idiomas() != null && !datosLibro.idiomas().isEmpty()) {
+            this.idioma = datosLibro.idiomas().get(0);
+        }
+        this.descargas = datosLibro.descargas();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Integer getDescargas() {
+        return descargas;
+    }
+
+    public void setDescargas(Integer descargas) {
+        this.descargas = descargas;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", autor='" + (autor != null ? autor.getNombre() : null) + '\'' +
+                ", idioma='" + idioma + '\'' +
+                ", descargas=" + descargas;
+    }
+
 }

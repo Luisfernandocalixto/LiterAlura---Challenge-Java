f41c7405932221f7cd543e28cdf36446de7d3447
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-20 20:19:26 -0600

    Preparando la clase Autor

--- src/main/java/com/alura/literatura/model/Autor.java
+++ src/main/java/com/alura/literatura/model/Autor.java
@@ -1,2 +1,93 @@
-package com.alura.literatura.model;public class Autor {
+package com.alura.literatura.model;
+
+import com.sun.source.doctree.AuthorTree;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private Integer anioNacimiento;
+    private Integer anioFallecimiento;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Libro> libros = new ArrayList<>();
+
+    // Constructores, getters y setters
+
+    public Autor() {
+    }
+
+    public Autor(DatosAutor datosAutor) {
+        this.nombre = datosAutor.nombre();
+        this.anioNacimiento = datosAutor.anioNacimiento();
+        this.anioFallecimiento = datosAutor.anioFallecimiento();
+    }
+
+    public void addLibro(Libro libro) {
+        libros.add(libro);
+        libro.setAutor(this);
+    }
+
+    public void removeLibro(Libro libro) {
+        libros.remove(libro);
+        libro.setAutor(null);
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getAnioNacimiento() {
+        return anioNacimiento;
+    }
+
+    public void setAnioNacimiento(Integer anioNacimiento) {
+        this.anioNacimiento = anioNacimiento;
+    }
+
+    public Integer getAnioFallecimiento() {
+        return anioFallecimiento;
+    }
+
+    public void setAnioFallecimiento(Integer anioFallecimiento) {
+        this.anioFallecimiento = anioFallecimiento;
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre='" + nombre + '\'' +
+                ", anioNacimiento=" + anioNacimiento +
+                ", anioFallecimiento=" + anioFallecimiento +
+                ", libros=" + libros;
+
+    }
 }

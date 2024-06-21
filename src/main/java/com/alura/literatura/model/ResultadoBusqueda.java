eac6c5f0d7f73753a702ba4d981395ac4230c673
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-20 20:20:36 -0600

    Preparando ResultadosBusqueda

--- src/main/java/com/alura/literatura/model/ResultadoBusqueda.java
+++ src/main/java/com/alura/literatura/model/ResultadoBusqueda.java
@@ -1,2 +1,45 @@
-package com.alura.literatura.model;public class ResultadoBusqueda {
+package com.alura.literatura.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResultadoBusqueda {
+    private int count;
+    private String next;
+    private String previuos;
+    private List<DatosLibro> results;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPreviuos() {
+        return previuos;
+    }
+
+    public void setPreviuos(String previuos) {
+        this.previuos = previuos;
+    }
+
+    public List<DatosLibro> getResults() {
+        return results;
+    }
+
+    public void setResults(List<DatosLibro> results) {
+        this.results = results;
+    }
 }

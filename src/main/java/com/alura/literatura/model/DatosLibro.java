a4a83a91ec2d3dc96c6a7316418f79d0cdb97e27
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-20 20:20:04 -0600

    Preparando DatosLibro

--- src/main/java/com/alura/literatura/model/DatosLibro.java
+++ src/main/java/com/alura/literatura/model/DatosLibro.java
@@ -1,2 +1,16 @@
-package com.alura.literatura.model;public record DatosLibro() {
+package com.alura.literatura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.web.JsonPath;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibro(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DatosAutor> autores,
+        @JsonAlias("languages") List<String> idiomas,
+        @JsonAlias("download_count") Integer descargas
+        ) {
 }

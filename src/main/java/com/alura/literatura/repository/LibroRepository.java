a25363279040b05be2440d4d76cf7a916e8a8bd7
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-06-20 20:21:47 -0600

    Ajustando la obtención de datos en LibroRepository

--- src/main/java/com/alura/literatura/repository/LibroRepository.java
+++ src/main/java/com/alura/literatura/repository/LibroRepository.java
@@ -1,2 +1,17 @@
-package com.alura.literatura.repository;public interface LibroRepository {
+package com.alura.literatura.repository;
+
+import com.alura.literatura.model.Autor;
+import com.alura.literatura.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    Optional<Libro> findByTituloAndAutorAndIdioma(String titulo, Autor autor, String idioma);
+
+    @Query("SELECT l FROM Libro l WHERE l.idioma =:idioma")
+    List<Libro> obtenerLibrosPorIdioma(String idioma);
 }
+
